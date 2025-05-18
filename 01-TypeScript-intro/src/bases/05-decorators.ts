e16be3496548616747a2dd6084099abcd8f3e895
Juan Esteban Camargo <juanes@cloudnonic.com>
2025-05-18 16:43:44 -0500

    docs: 📝 Decorators - Typescript intro

---
+++ 01-TypeScript-intro/src/bases/05-decorators.ts
@@ -0,0 +1,48 @@
+class NewPokemon {
+    public readonly id: number;
+    public name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+    scream() {
+        console.log('no quiero gritar!');
+    }
+
+    speak() {
+        console.log('no quiero hablar!');
+    }
+}
+
+const MyDecorator = () => {
+    return (target: Function) => {
+        // console.log(target)
+        return NewPokemon
+    }
+}
+
+@MyDecorator()
+export class Pokemon {
+    public readonly id: number;
+    public name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+    scream() {
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+
+    speak() {
+        console.log(`${this.name}, ${this.name}!`);
+    }
+}
+
+export const charmander = new Pokemon(4, 'Charmander')
+
+charmander.scream()
+charmander.speak()
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import {charmander} from "./bases/04-injection.ts";
+import {charmander} from "./bases/05-decorators.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
