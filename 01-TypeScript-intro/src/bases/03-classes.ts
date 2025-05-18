2a30158541d5c6b4ddcebad9cea7869281ae28ba
Juan Esteban Camargo <juanes@cloudnonic.com>
2025-05-18 07:41:01 -0500

    docs: 📝 Methods and Getters - Typescript intro

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -9,7 +9,31 @@ export class PokemonA {
 }
 
 export class PokemonB {
-    constructor(public readonly id: number, public name: string) {}
+
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.png`;
+    }
+
+    constructor(
+        // @ts-ignore
+        public readonly id: number,
+        // @ts-ignore
+        public name: string
+    ) {}
+
+    //methods
+
+    public scream() {
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+
+    public speak() {
+        console.log(`${this.name}, ${this.name}`);
+    }
 }
 
 export const charmander = new PokemonA(4, 'Charmander');
+export const pikachu = new PokemonB(5, 'Pikachu');
+
+pikachu.scream();
+pikachu.speak();
